6fa1a8382b24e36c45e3ef005a8a542ed00458f8
Korbin-Dansie <Dansiekm@gmail.com>
2019-11-23 10:43:28 -0700

    Link pages

--- server.js
+++ server.js
@@ -10,35 +10,33 @@ const port = process.env.PORT || 5000;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// index page --Reads file index.html
-app.get('/', (req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html"
-  });
-  res.write("Main File");
-  res.end();
-});
+/**********************************************************
+ * Home Page
+ **********************************************************/
+app.get('/', readIndexFile);
 
-
-//Set up /api/
+/**********************************************************
+ * Route for api
+ **********************************************************/
 const api = require('./routes/api');
 app.use('/api/', api);
 
-
-//For unkown pages
-// ! Do not put other pages after this
+/**********************************************************
+ * For Unkown pages
+ * ! Do not put other pages after this
+ **********************************************************/
 app.use('*', readUnkownPage);
 
-
-
-
-//Include images
+/**********************************************************
+ * Include folders
+ **********************************************************/
 app.use('/img', express.static(path.join(__dirname, 'public/img')))
 app.use('/css', express.static(path.join(__dirname, 'public/css')))
-
-//Test Folder
 app.use('/test', express.static(path.join(__dirname, 'Test')))
 
+/**********************************************************
+ * Start listening on port
+ **********************************************************/
 app.listen(port);
 console.log("listening on port " + port);
 
@@ -52,11 +50,27 @@ function readUnkownPage(req, res) {
   fs.readFile(location, 'utf8', (err, data) => {
     if (err) {
       //console.error(err.name + ': ' + err.message);
-      res.writeHead(200, {
+      res.writeHead(404, {
         "Content-Type": "text/html"
       });
       res.write("Error Unable To read file. At" + location);
       res.end();
+      return;
+    }
+    res.writeHead(404, {
+      "Content-Type": "text/html"
+    });
+    res.write(data);
+    res.end();
+  });
+}
+
+function readIndexFile(req, res) {
+  //console.log("Current path is: "+ path.join(__dirname));
+  fs.readFile(path.join(__dirname, 'views/pages/index.html'), 'utf8', (err, data) => {
+    if (err) {
+      console.error("Error Reading file", err);
+      readUnkownPage(req, res);
       return;
     }
     res.writeHead(200, {
